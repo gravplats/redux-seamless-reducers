c7937d1af09416ebeaebbc471e45b2af5c402170
Mattias Rydengren <mattias.rydengren@coderesque.com>
2016-12-31 00:39:04 +0100

    Delegate to redux-actions.

--- modules/index.js
+++ modules/index.js
@@ -1,33 +1,8 @@
-import { isError } from 'flux-standard-action';
-import reduceReducers from 'reduce-reducers';
+import { handleActions } from 'redux-actions';
 import Immutable from 'seamless-immutable';
 
 
-const isFunction = (obj) => typeof obj === 'function';
-
-const handle = (type, reducers) => {
-    return (state, action) => {
-        if (action.type !== type) {
-            return state;
-        }
-
-        const handlerKey = isError(action)
-            ? 'throw'
-            : 'next';
-
-        if (isFunction(reducers)) {
-            reducers.next = reducers;
-        }
-
-        const reducer = reducers[handlerKey];
-
-        return isFunction(reducer)
-            ? Immutable(reducer(state, action))
-            : state;
-    };
-};
-
-export default (reducers, defaultState = {}) => {
-    const handlers = Object.keys(reducers).map((type) => handle(type, reducers[type]));
-    return (state = Immutable(defaultState), action) => reduceReducers(...handlers)(state, action);
+export default (handlers, defaultState = {}) => {
+    const reducer = handleActions(handlers, Immutable(defaultState));
+    return (state, action) => Immutable(reducer(state, action));
 };
